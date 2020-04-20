3df5ffc8507aec27e6ccb1875df3b9fff46d4128
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-04-19 17:46:15 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,20 @@
-export function manageFriends(state, action){
+
+
+export function manageFriends(state = {friends:[]}, action){
+   
+        
+            switch (action.type){
+        case 'ADD_FRIEND':
+        return  {...state, friends: [...state.friends, action.friend] }
+        case 'REMOVE_FRIEND':
+        return {...state, friends: state.friends.filter(x => {
+            return x.id != action.id;
+          })}
+        default:
+        return state;
+        
+            }
+      
+
+
 }
