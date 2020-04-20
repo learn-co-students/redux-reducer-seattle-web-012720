9448a8dd2c0ef1a84ba383ede23fb14fea1916cd
kailana <kailanakahawaii@gmail.com>
2020-04-20 10:50:55 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
-export function manageFriends(state, action){
+export function manageFriends(state = initialState, action){
+
+    const initialState = {
+        friends: []
+    }
+
+    switch(action.type){
+        case 'ADD_FRIEND': 
+            return {
+                ...state,
+                friends: [...state.friends, action.friend]
+            }
+        case 'REMOVE_FRIEND': 
+            return { 
+                ...state, 
+                friends: state.friends.filter(friend => friend.id !== action.id)
+            }
+        default: 
+            return state;
+    }
 }
+
+
+
