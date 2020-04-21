19fad810ea2662dc3f72ff603a8fdb606950a451
unclezap <zeb.blackwell@gmail.com>
2020-04-21 01:48:32 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,25 @@
 export function manageFriends(state, action){
+    // state = {...state, friends: []}
+    switch (action.type) {
+        case "ADD_FRIEND":
+            state = Object.assign({}, state, {friends: [...state.friends, action.friend]})
+        break;
+        case "REMOVE_FRIEND":
+            state = Object.assign({}, state, {friends: state.friends.filter(friend => friend.id !== action.id)})
+        break;
+        default: return state
+
+    }
+    return state
 }
+
+
+// export function managePresents(state, action){
+//     state = {...state, 
+//         numberOfPresents: 0
+//     }
+//     if (action.type === "INCREASE") {
+//         state = {...state, numberOfPresents: state.numberOfPresents + 1}
+//     }
+//     return state
+// }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,9 @@
 export function managePresents(state, action){
+    state = {...state, 
+        numberOfPresents: 0
+    }
+    if (action.type === "INCREASE") {
+        state = {...state, numberOfPresents: state.numberOfPresents + 1}
+    }
+    return state
 }
